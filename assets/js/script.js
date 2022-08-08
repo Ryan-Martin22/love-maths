6d7704324c723329e4e1a499f7d895d458e3793b
Ryan-Martin22 <ryan_martindlk@hotmail.com>
2022-08-08 12:04:05 +0000

    added addition game

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,10 +7,12 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You Clicked Submit");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert (`You Clicked ${gameType}`)
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
 
                  
@@ -18,11 +20,18 @@ document.addEventListener("DOMContentLoaded", function() {
  * the main game "loop", called when the game is frist loaded
  * and after the users answer has been prosessed
  */
-        function runGame() {
+        function runGame(gameType) {
 
             // creates two random numbers between 1 and 25
             let num1 = Math.floor(Math.random() * 25) + 1;
             let num2 = Math.floor(Math.random() * 25) + 1;
+
+            if (gameType === "addition") {
+                displayAdditionQuestion(num1, num2 )
+            } else {
+                alert(`Unknown game type: ${gameType}`);
+                throw `Unknown game type: ${gameType}. Aborting!` ; 
+            }
         }
 
         function checkAnswer() {
@@ -41,8 +50,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
         }
 
-        function displayAdditionQuestion() {
-
+        function displayAdditionQuestion(operand1, operand2) {
+             document.getElementById('operand1').textContent = operand1;
+             document.getElementById('operand2').textContent = operand2;
+             document.getElementById('operator').textContent = "+"
         }
 
         function displaySubtractQuestion() {
